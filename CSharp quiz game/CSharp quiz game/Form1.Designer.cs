4043002a939e5891910d3b130481bde228fa9f7c
Raphael Henrique <raphaelhenriquedev@gmail.com>
2022-09-25 21:47:31 -0300

    Primeira Versao

---
+++ CSharp quiz game/CSharp quiz game/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CSharp_quiz_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSharp_quiz_game.Properties.Resources.imgImprimir;
+            this.pictureBox1.Location = new System.Drawing.Point(237, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 302);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Myanmar Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(106, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Myanmar Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(529, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(286, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Myanmar Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(106, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(286, 61);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Myanmar Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(529, 427);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(286, 61);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblQuestion.Location = new System.Drawing.Point(12, 294);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(861, 45);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "Question One";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(885, 521);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblQuestion;
+    }
+}
+
---
+++ CSharp quiz game/CSharp quiz game/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_quiz_game
+{
+    public partial class Form1 : Form
+    {
+
+        // Variáveis do jogo
+        int correctAnswer, questionNumber = 1, score = 0, percentage, totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 7;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if(buttonTag == correctAnswer)
+            {
+                score++;
+            }
+            if(questionNumber == totalQuestions)
+            {
+                // Aqui vai aparecer a porcentagem de acertos
+
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+
+                MessageBox.Show(
+                    "O quiz chegou ao fim!" + Environment.NewLine + 
+                    "Você acertou corretamente " + score + " questões" + Environment.NewLine +
+                    "Sua porcentagem de acertos foi de: " + percentage + "%" + Environment.NewLine +
+                    "Clique em OK para jogar novamente."
+
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+
+            }
+
+
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+
+        private void askQuestion(int numQuest) // Método que mostrará corretamente na tela a image, label e buttons referentes a cada pergunta.
+        {
+            switch (questionNumber)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.imgImprimir; // "img.Imprimir é o nome da imagem"
+
+                    lblQuestion.Text = "Questão 1";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "Console.WriteText()"; 
+                    button2.Text = "WriteLine()";
+                    button3.Text = "Console.WriteLine()";
+                    button4.Text = "Console.TextLine()";
+
+                    // A resposta correta é a 3, logo, o botão com tag 3.
+                    correctAnswer = 3;
+                    
+                    break;
+                case 2:
+
+                    pictureBox1.Image = Properties.Resources.imgLer; // "img.... é o nome da imagem"
+
+                    lblQuestion.Text = "Questão 2";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "Console.ReadText()";
+                    button2.Text = "ReadLine()";
+                    button3.Text = "Console.WriteLine()";
+                    button4.Text = "Console.ReadLine()";
+
+                    // A resposta correta é a 4, logo, o botão com tag 4.
+                    correctAnswer = 4;
+
+                    break;
+                case 3:
+
+                    pictureBox1.Image = Properties.Resources.imgDeclaraConst; // "img.... é o nome da imagem"
+
+                    lblQuestion.Text = "Questão 3";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "def constante = NOME_DA_CONST";
+                    button2.Text = "const int NOME_DA_CONST";
+                    button3.Text = "#define constant = NOME_DA_CONST";
+                    button4.Text = "int const NOME_DA_CONST";
+                    
+                    correctAnswer = 2;
+
+                    break;
+                case 4:
+                    
+                    pictureBox1.Image = Properties.Resources.imgCriarClass; // "img.... é o nome da imagem"
+                    lblQuestion.Text = "Questão 4";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "create class NomeDaClass";
+                    button2.Text = "definirTipo class NomeDaClass()";                    
+                    button3.Text = "visibilidade class NomeDaClass()";
+                    button4.Text = "visibilidade class NomeDaClass";
+                    
+                    correctAnswer = 4;
+
+                    break;
+                case 5:
+
+                    pictureBox1.Image = Properties.Resources.imgVisibClass; // "img.... é o nome da imagem"
+                    lblQuestion.Text = "Questão 5";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "public, private, static";
+                    button2.Text = "int, double, char";
+                    button3.Text = "define,main, void";
+                    button4.Text = "private, void, long";
+                    
+                    correctAnswer = 1;
+
+                    break;
+                case 6:
+
+                    pictureBox1.Image = Properties.Resources.imgCriarMet; // "img.... é o nome da imagem"
+
+                    lblQuestion.Text = "Questão 6";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "tipoDoMet NomeDoMet(param)";
+                    button2.Text = "visibilidade met NomeDoMet";
+                    button3.Text = "visibilidade NomeDoMet()";
+                    button4.Text = "class NomeDoMet()";
+                    
+                    correctAnswer = 1;
+
+                    break;
+
+                case 7:
+                    
+                    pictureBox1.Image = Properties.Resources.imgInstObj; // "img.... é o nome da imagem"
+
+                    lblQuestion.Text = "Questão 7";
+
+                    // Cada um desses botões possue uma tag(1,2,3,4)
+                    button1.Text = "objectName ClassName = new tipo;";
+                    button2.Text = "ClassName objectName = new ClassName(); ";
+                    button3.Text = "tipo objectName();";
+                    button4.Text = "ClassName tipo objectName = new ClassName;";
+                    
+                    correctAnswer = 2;
+
+                    break;
+
+            }
+        }
+
+    }
+}
+                    
